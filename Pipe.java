77cfafa3e22140c8ff294b06c3fe7a6220989965
Nathan <nwiley@uco.edu>
2025-02-25 19:02:38 -0600

    Add functions for Pipe class

--- Pipe.java
+++ Pipe.java
@@ -6,4 +6,30 @@ class Pipe {
     private List<Line> buffer = new ArrayList<>();
     private boolean closed = false;
 
+    public synchronized void putLine(Line line) throws InterruptedException {
+        while (buffer.size() >= 100) { // Buffer size limit
+            wait();
+        }
+        buffer.add(line);
+        notifyAll();
+    }
+
+    public synchronized Line getLine() throws InterruptedException {
+        while (buffer.isEmpty() && !closed) {
+            wait();
+        }
+
+        if (buffer.isEmpty() && closed) {
+            return null; // Signal end of input
+        }
+
+        Line line = buffer.remove(0);
+        notifyAll();
+        return line;
+    }
+
+    public synchronized void closeForWriting() {
+        closed = true;
+        notifyAll();
+    }
 }
