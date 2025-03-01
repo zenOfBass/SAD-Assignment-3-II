b2e34328e34582c7cbcec25fc1d30af7dea9cfc5
Nathan <nwiley@uco.edu>
2025-03-01 13:50:49 -0600

    Add run() OutputFormatter

--- OutputFormatter.java
+++ OutputFormatter.java
@@ -7,7 +7,20 @@ class OutputFormatter implements Filter {
 
     @Override
     public void run() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'run'");
+        try {
+            System.out.println("KWIC Index System Output:");
+            System.out.println("-------------------------");
+
+            Line line;
+            int count = 1;
+            while ((line = input.getLine()) != null) {
+                System.out.println(count + ". " + line);
+                count++;
+            }
+
+            System.out.println("-------------------------");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
